db212891d2ab03d00dbf3c073844943da96fdee8
Anshu Garg <a.kgarg9050@gmail.com>
2022-07-03 22:01:01 +0530

    Update .gitignore

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome To Gathbandhan.com!</Text>
       <StatusBar style="auto" />
     </View>
   );
